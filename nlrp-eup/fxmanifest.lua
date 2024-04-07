722c1a746135cac3f76366b8989dcb06b735358e
BandzUK <165606804+BandzUK@users.noreply.github.com>
2024-04-07 10:07:49 -0700

    constable + pcso clothing

--- nlrp-eup/fxmanifest.lua
+++ nlrp-eup/fxmanifest.lua
@@ -4,11 +4,9 @@ fx_version 'cerulean'
 game { 'gta5' }
 
 files {
-  'mp_m_freemode_01_mp_m_autobundle.meta',
-  'mp_f_freemode_01_mp_f_autobundle.meta',
+  'mp_m_freemode_01_mp_m_mbmvip2709.meta',
   'stream/pedalternatevariations.meta'
 }
 
-data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_autobundle.meta'
-data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_autobundle.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_mbmvip2709.meta'
 data_file 'ALTERNATE_VARIATIONS_FILE' 'stream/pedalternatevariations.meta'
